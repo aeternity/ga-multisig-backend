7ee6f08b578e0a2425831113be1f13418b9996b8
Philipp Piwo <github@piwo.space>
2022-08-23 09:19:00 +0200

    use postgres instead of sqlite

--- src/db/db.js
+++ src/db/db.js
@@ -1,9 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-});
+const sequelize = new Sequelize(process.env.PG_CONNECTION_URI || 'postgres://postgres:postgres@localhost:5432/postgres');
 
 module.exports = {
   sequelize,
