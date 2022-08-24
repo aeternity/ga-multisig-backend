50fd9e8977334854512c4a3d325e8e8647b72558
Philipp Piwo <github@piwo.space>
2022-08-24 08:57:45 +0200

    move db config to .env

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,12 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.PG_CONNECTION_URI || 'postgres://postgres:postgres@localhost:5432/postgres');
+if(!process.env.PG_USER) throw new Error("PG_USER Environment Missing");
+if(!process.env.PG_PASSWORD) throw new Error("PG_PASSWORD Environment Missing");
+if(!process.env.PG_HOST) throw new Error("PG_HOST Environment Missing");
+if(!process.env.PG_PORT) throw new Error("PG_PORT Environment Missing");
+if(!process.env.PG_DB) throw new Error("PG_DB Environment Missing");
+
+const sequelize = new Sequelize(`postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DB}`);
 
 module.exports = {
   sequelize,
