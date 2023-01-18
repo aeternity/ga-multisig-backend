b2cc0e9455dd1db8cd54caef26defa09e6212fff
Philipp <github@piwo.space>
2023-01-18 14:05:16 +0100

    chore!: rename db fields, improve error handling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const cron = require('node-cron');
-const { indexSigners, initClient, nextHeight, initWebsocket, createDBIfNotExists } = require('./logic');
+const { indexSigners, initClient, nextHeight, initWebsocket, createDBIfNotExists, createTransaction } = require('./logic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -7,8 +7,11 @@ const cors = require('cors');
 const Signer = require('./db/Signer');
 const { Op } = require('sequelize');
 const Tx = require('./db/Tx');
+const { isAddressValid } = require('@aeternity/aepp-sdk');
+const { TxUnpackFailedError, TxHashNotMatchingError, HashAlreadyExistentError } = require('./util');
 
 let running = true;
+let status = 'started';
 
 process.on('unhandledRejection', (e) => {
   console.log('unhandledRejection', e);
@@ -28,26 +31,26 @@ const sync = (height) => {
     });
 };
 
+const initialize = async () => {
+  await createDBIfNotExists();
+  //await cleanDB();
+  await initClient();
+
+  // sync first, then init websocket
+  await sync(await nextHeight());
+  await initWebsocket();
+
+  // sync periodically to ensure latest info
+  cron.schedule('* * * * *', async () => {
+    if (!running) {
+      running = true;
+      await sync(await nextHeight());
+    } else console.log('already running');
+  });
+};
+
 const start = async () => {
-  const initialize = async () => {
-    await createDBIfNotExists();
-    //await cleanDB();
-    await initClient();
-
-    // sync first, then init websocket
-    await sync(await nextHeight());
-    await initWebsocket();
-
-    // sync periodically to ensure latest info
-    cron.schedule('* * * * *', async () => {
-      if (!running) {
-        running = true;
-        await sync(await nextHeight());
-      } else console.log('already running');
-    });
-  };
-
-  void initialize();
+  void initialize().then(() => (status = 'synced'));
 
   const app = express();
   const port = 3000;
@@ -56,31 +59,51 @@ const start = async () => {
   app.use(bodyParser.json());
 
   app.get('/health', async (req, res) => {
-    res.send('healthy');
+    return res.json({ status });
   });
 
   app.post('/tx', async (req, res) => {
-    await Tx.create({ hash: req.body.hash, data: req.body.data })
+    if (!req.body.hash || !req.body.tx) {
+      res.status(400);
+      return res.json({ error: 'request body has to contain hash and tx' });
+    }
+
+    return createTransaction(req.body.hash, req.body.tx)
       .then(() => res.sendStatus(204))
       .catch((e) => {
-        if (e.errors?.some((e) => e.validatorKey === 'not_unique')) res.sendStatus(409);
-        else {
+        if (e instanceof HashAlreadyExistentError) {
+          res.status(409);
+          return res.json({ error: e.message });
+        } else if (e instanceof TxUnpackFailedError || e instanceof TxHashNotMatchingError) {
+          res.status(400);
+          return res.json({ error: e.message });
+        } else {
           console.error(e);
-          res.sendStatus(500);
+          return res.sendStatus(500);
         }
       });
   });
 
   app.get('/tx/:hash', async (req, res) => {
+    if (!req.params.hash) {
+      res.status(400);
+      return res.json({ error: 'request has to be in format /tx/:hash' });
+    }
     const tx = await Tx.findOne({ where: { hash: req.params.hash } });
-    if (tx) res.send(tx);
-    else res.sendStatus(404);
+
+    if (tx) return res.json(tx);
+    else return res.sendStatus(404);
   });
 
   app.get('/:signerId', async (req, res) => {
-    let fromHeight = req.query.fromHeight;
+    if (!req.params.signerId || !isAddressValid(req.params.signerId)) {
+      res.status(400);
+      return res.json({ error: 'request has to be in format /:signerId and valid signer account' });
+    }
 
-    res.send(
+    const fromHeight = req.query.fromHeight;
+
+    return res.json(
       await Signer.findAll({
         where: {
           ...(fromHeight ? { height: { [Op.gte]: fromHeight } } : {}),
@@ -91,7 +114,7 @@ const start = async () => {
   });
 
   app.get('/', async (req, res) => {
-    res.send(await Signer.findAll());
+    return res.json(await Signer.findAll());
   });
 
   app.listen(port, () => {
